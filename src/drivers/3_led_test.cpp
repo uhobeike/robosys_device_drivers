500213f1c471911d9bccd31280fd54002ef53b7a
uhobeike <tatuhiro315@ezweb.ne.jp>
2020-12-08 03:18:44 +0900

    all dev check OK

--- src/drivers/3_led_test.cpp
+++ src/drivers/3_led_test.cpp
@@ -3,8 +3,8 @@
 #include <string>
 #include "unistd.h"
 
-#define FILE_LCD10          "/dev/lcd_row_10"
-#define FILE_LCD20          "/dev/lcd0_row_20"
+#define FILE_LCD_1          "/dev/lcd_row_10"
+#define FILE_LCD_2          "/dev/lcd0_row_20"
 #define FILE_LCD_CLEAR      "/dev/lcd_clear0"
 #define FILE_ANALOG_READ    "/dev/analog_read0"
 #define FILE_LED_BLINK0     "/dev/led_blink0"
@@ -13,25 +13,79 @@
 
 #define BUFF_SIZE 256
 
-int analog_read()
-{
-    std::ifstream ifs(FILE_ANALOG_READ);
-    std::string str;
+using std::string;
+using std::ifstream;
+using std::ofstream;
+using std::cout;
+using std::endl;
 
-    if (ifs.fail()){
-        std::cerr << "Failed to open file." << std::endl;
-        return -1;
+void lcd_clear()
+{
+    std::ofstream ofs(FILE_LCD_CLEAR);
+    
+    ofs << std::endl;
+}
+
+void lcd_write_10(string lcd_str)
+{
+    std::ofstream ofs(FILE_LCD_1);
+    
+    ofs << lcd_str;
+}
+
+void lcd_write_20(string &lcd_str)
+{
+    std::ofstream ofs(FILE_LCD_2);
+    
+    ofs << lcd_str;
+}
+
+string analog_read()
+{
+    ifstream ifs(FILE_ANALOG_READ);
+    string str;
+
+    getline(ifs, str);
+    cout << str << endl;
+
+    return str;
+}
+
+void led_blink(char status_0, char status_1, char status_2)
+{
+    std::ofstream ofs_0(FILE_LED_BLINK0);
+    std::ofstream ofs_1(FILE_LED_BLINK1);
+    std::ofstream ofs_2(FILE_LED_BLINK2);
+
+    if (status_0){
+        ofs_0 << 1;
+    } else if (!status_0){
+        ofs_0 << 0;
     }
 
-    while (getline(ifs, str)) {
-        std::cout << str << std::endl;
-        usleep(1000);//スリープ入れないと負荷率高くて音が出る
+    if (status_1){
+        ofs_1 << 1;
+    } else if (!status_1){
+        ofs_1 << 0;
+    }
+
+    if (status_2){
+        ofs_2 << 1;
+    } else if (!status_2){
+        ofs_2 << 0;
     }
 }
 
 int main(void) 
 {
+    /* lcdの初期化*/
+    lcd_clear();
+
     analog_read();
+
+    lcd_write_10("114514");
+
+    led_blink(0, 0, 0);
 
     return 0;
 }
